77bb7301186762d12bcc5c2bca779f361ca4dca1
Alexander Kramer <a.kramer@reply.de>
2023-01-06 17:52:22 +0100

    :bento: add auth controller and service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return { msg: 'I am signed up' };
+  }
+
+  login() {
+    return { msg: 'I am logged in' };
+  }
+}
