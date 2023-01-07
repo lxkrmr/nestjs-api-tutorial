59aa359ca5c0279ab74dbc01fce8c67e21f77f03
Alexander Kramer <a.kramer@reply.de>
2023-01-07 09:49:43 +0100

    :bento: ensure unique email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { User } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto): Promise<Partial<User>> {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
 
-    // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // TODO: Replace with transformer
-    delete user.hash;
+      // TODO: Replace with transformer
+      delete user.hash;
 
-    // return the save user
-    return user;
+      // return the save user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 
   login() {
