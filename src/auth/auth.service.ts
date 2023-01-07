cb8d2e6796beb0b18625ccfdd8f51bb6c3ffade4
Alexander Kramer <a.kramer@reply.de>
2023-01-07 09:27:39 +0100

    :sparkles: encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
-    return { dto };
+  async signup(dto: AuthDto): Promise<Partial<User>> {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    // return the save user
+    return user;
   }
 
   login() {
